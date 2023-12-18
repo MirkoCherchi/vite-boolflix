f491d098ef38d46a14e15e8620fa77d841c82ef4
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-12-18 15:49:33 +0100

    fix: add comment

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
+<!-- App.vue -->
+
 <script>
+// Importa il componente AppHeader dal percorso specificato
 import AppHeader from "./components/header/AppHeader.vue";
+// Importa il componente AppMain dal percorso specificato
 import AppMain from "./components/main/AppMain.vue";
+// Importa lo store globale e Axios
 import { store } from "./store";
 import axios from "axios";
 
 export default {
+    // Definizione dei dati del componente
     data() {
         return {
+            // Utilizza lo store come dato del componente
             store,
         };
     },
+    // Definizione dei metodi del componente
     methods: {
+        // Metodo chiamato quando viene eseguita una ricerca
         search() {
+            // Esegue una richiesta API per i film utilizzando Axios
             axios
                 .get(store.config.urlFilm, {
                     params: {
@@ -20,11 +30,13 @@ export default {
                     },
                 })
                 .then((response) => {
+                    // Logga i risultati nella console (a scopo di debug)
                     console.log(response.data.results);
-
+                    // Aggiorna i risultati della ricerca dei film nello store
                     this.store.searchResultFilm = response.data.results;
                 });
 
+            // Esegue una richiesta API per le serie TV utilizzando Axios
             axios
                 .get(store.config.urlTv, {
                     params: {
@@ -33,12 +45,16 @@ export default {
                     },
                 })
                 .then((response) => {
+                    // Logga i risultati nella console (a scopo di debug)
                     console.log(response.data.results);
+                    // Aggiorna i risultati della ricerca delle serie TV nello store
                     this.store.searchResultTv = response.data.results;
                 });
         },
     },
+    // Definizione dei componenti utilizzati
     components: {
+        // Dichiarazione dei componenti utilizzati nel template
         AppHeader,
         AppMain,
     },
@@ -46,10 +62,13 @@ export default {
 </script>
 
 <template>
+    <!-- Utilizza il componente AppHeader e passa il metodo search come evento -->
     <AppHeader @search="search" />
+    <!-- Utilizza il componente AppMain -->
     <AppMain />
 </template>
 
 <style lang="scss">
+// Importa lo stile globale dal percorso specificato
 @use "../src/assets/styles/style.scss";
 </style>
